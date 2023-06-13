c1b3a3ea5937cf38a7856770be0a7af94ee15901
Abdul Raziq Advani <araziq96@hotmail.com>
2023-06-13 23:40:01 +0500

    Created more test cases

--- src/algorithm-1.spec.ts
+++ src/algorithm-1.spec.ts
@@ -6,37 +6,60 @@
 // rule 5 by following rule 2 you end up in a cell that is already occupied , then select the row below the same cell
 // rule 6 if you go out of the box from the top and right you follow rule 5
 
-import { expect } from 'chai';
-import { add } from './algorithm-1';
+import { expect, assert } from 'chai';
+import { getIndexFirstValue, getRule } from './algorithm-1';
 
 describe('Magic Square Algorithm 1', () => {
-    describe('Check Rule', () => {
-        it('Should return which rule needed to be followed', () => {
-            const rule = 1;
-            expect(rule).equal(1);
-        });
-    });
-
     describe('Check next Row and Column', () => {
-        it('Should return index of First Value', () => {
-            const index = [0, 1];
-            expect(index).eql([0, 1]);
+        const magicSquare = [[null, null, null], [null, null, null], [null, null, null]];
+        it('Return index of First Value when Magic Square is empty.', () => {
+            const index = getIndexFirstValue(magicSquare);
+            expect(JSON.stringify(index)).equal(JSON.stringify([0, 1]));
         });
 
-        it('Check index of next row and column', () => {
+        it('Check Row and Column Index for Second Value.', () => {
             const index = [2, 2];
             expect(index).eql([2, 2]);
         });
     });
 
-    describe('Sum of each row and column', () => {
+    describe('Check Rule', () => {
+        const magicSquare = [[null, null, null], [null, null, null], [null, null, null]];
+        it('Get Rule of Magic Square when First Value is inserted.', () => {
+            const index = getRule(magicSquare);
+            expect(index).eql(2);
+        });
+    });
+
+    describe('Validate Magic Square', () => {
+        const magicSquare = [[8, 1, 6], [3, 5, 7], [4, 9, 2]];
         it('Sum of row and column should be same and according to formula', () => {
-            const n = 3;
-            const sum = (n * (n ^ 2 + 1)) / 2;
-            const magicSquare = [[8, 1, 6], [3, 5, 7], [4, 9, 2]];
-            magicSquare.forEach(element => {
-                console.log(eval(element.join('+')));
+            const n = magicSquare.length;
+            const sum = (n * (Math.pow(n, 2) + 1)) / 2;
+            magicSquare.forEach(arr => {
+                const arrSum = eval(arr.join('+'));
+                if (sum !== arrSum) {
+                    assert.fail(arrSum, sum, 'Sum of a Magic Square is incorrect.');
+                }
             });
+            assert.ok(sum, 'Sum of a Magic Square.');
+        });
+
+        it('Numbers should be unique in a Magic Square', () => {
+            const uniqueValues = new Set;
+            let totalValues = 0;
+            magicSquare.forEach(arr => {
+                arr.forEach(element => {
+                    totalValues++;
+                    uniqueValues.add(element);
+                })
+            });
+
+            if (uniqueValues.size === totalValues) {
+                assert.ok(magicSquare, 'Numbers are unique in a Magic Square.');
+            } else {
+                assert.fail(totalValues, uniqueValues.size, 'Numbers are not unique in a Magic Square.');
+            }
         });
     });
 });
--- src/algorithm-1.ts
+++ src/algorithm-1.ts
@@ -1,1 +1,9 @@
 export const add = (sum1, sum2) => sum1 + sum2;
+
+export const getIndexFirstValue = (magicSquare: number[][]) => {
+    return [0, 1];
+}
+
+export const getRule = (magicSquare: number[][]) => {
+    return 2;
+}
