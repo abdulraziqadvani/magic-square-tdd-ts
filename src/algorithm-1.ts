56e8846bd9d2570290181dc50a6c9ddb0a5cb587
Abdul Raziq Advani <araziq96@hotmail.com>
2023-07-25 22:28:31 +0500

    Code refactored

--- src/algorithm-1.ts
+++
@@ -1,9 +0,0 @@
-export const add = (sum1, sum2) => sum1 + sum2;
-
-export const getIndexFirstValue = (magicSquare: number[][]) => {
-    return [0, 1];
-}
-
-export const getRule = (magicSquare: number[][]) => {
-    return 2;
-}
